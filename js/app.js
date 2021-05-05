4962d689a0ec4cd6e79674871a4331cc1bc50ee6
florian <martinflorian2@gmail.com>
2021-05-05 15:35:46 +0200

    canva 8*8 created

--- js/app.js
+++ js/app.js
@@ -1,1 +1,26 @@
+// Create table element
+const tableElement = document.createElement('table');
+
+//insert table element
+const invaderElement = document.getElementById('invader');
+invaderElement.appendChild(tableElement);
+
 // ton code ici
+const canvaHeight = 8;
+const canvaWidth = 8;
+
+// select all table rows
+
+for (i = 0; i < canvaWidth; i++) {
+    // debugger;
+    const rowElement = document.createElement('tr');
+    rowElement.className = 'rowElement';
+    tableElement.appendChild(rowElement);
+
+    for (j = 0; j < canvaHeight; j++) {
+        const cellElement = document.createElement('td');
+        cellElement.className = 'cellElement';
+        rowElement.appendChild(cellElement);
+    }
+
+}
