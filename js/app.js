e1485cd87f637523adde26f6ac2cf35f5b59e44e
florian <martinflorian2@gmail.com>
2021-05-06 11:09:53 +0200

    grid generation and cells bg color ok

--- js/app.js
+++ js/app.js
@@ -1,26 +1,43 @@
-// Create table element
-const tableElement = document.createElement('table');
+// grid value
+const canvaSize = 8;
 
 //insert table element
 const invaderElement = document.getElementById('invader');
-invaderElement.appendChild(tableElement);
 
-// ton code ici
-const canvaHeight = 8;
-const canvaWidth = 8;
-
-// select all table rows
-
-for (i = 0; i < canvaWidth; i++) {
+// Creates grid
+for (i = 0; i < canvaSize; i++) {
     // debugger;
-    const rowElement = document.createElement('tr');
+    const rowElement = document.createElement('div');
     rowElement.className = 'rowElement';
-    tableElement.appendChild(rowElement);
 
-    for (j = 0; j < canvaHeight; j++) {
-        const cellElement = document.createElement('td');
+    for (j = 0; j < canvaSize; j++) {
+        const cellElement = document.createElement('div');
         cellElement.className = 'cellElement';
         rowElement.appendChild(cellElement);
+        cellElement.addEventListener('mouseup', function () {
+            // cellElement.style.backgroundColor = curentColor === 'black' ? 'black' : "white";
+            if (cellElement.style.backgroundColor === "black") {
+                cellElement.style.backgroundColor = "white";
+            }
+            else {
+                cellElement.style.backgroundColor = "black";
+            }
+        })
     }
+    invaderElement.appendChild(rowElement);
+};
+// debugger;
+// select form element
+const configElement = document.querySelector('.configuration');
 
-}
+// create input type number and ad it to the form
+const numberElement = document.createElement('input');
+numberElement.setAttribute('type', 'number');
+numberElement.setAttribute('value', '16');
+numberElement.setAttribute('min', '4');
+configElement.appendChild(numberElement);
+
+const validationElement = document.createElement('input');
+validationElement.setAttribute('type', 'button');
+validationElement.setAttribute('value', 'Apply');
+configElement.appendChild(validationElement);
