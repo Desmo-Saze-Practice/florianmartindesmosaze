a086745b4ce69949d416cddc028e436bb42561f6
florian <martinflorian2@gmail.com>
2021-05-25 12:24:41 +0200

    finished all bonus, added color selection

--- js/app.js
+++ js/app.js
@@ -1,75 +1,120 @@
 const grid = {
+    // Settings part
     gridSize: 8,
+    pixelSize: 10,
+    colorValue: '#000',
+    footerElement: document.querySelector('.bottom-page'),
+    configElement: document.querySelector('.configuration'),
+    invaderElement: document.getElementById('invader'),
+    numberElement: document.createElement('input'),
+    validationElement: document.createElement('button'),
+    pixelInputElement: document.createElement('input'),
+    colorPickElement: document.createElement('input'),
+    bluevioletElement: document.querySelector('.blueviolet'),
+    blackElement: document.querySelector('.black'),
+
+    // functions part
     resetGrid: function () {
         document.getElementById('invader').innerHTML = '';
     },
+
     generateInput: function () {
-        // select form element
-        const configElement = document.querySelector('.configuration');
+        // input type number for grid size
+        grid.numberElement.setAttribute('class', 'numberInput');
+        grid.numberElement.setAttribute('type', 'number');
+        grid.numberElement.setAttribute('min', '4');
+        grid.numberElement.setAttribute('max', '200');
+        grid.numberElement.setAttribute('value', '16');
+        grid.configElement.appendChild(grid.numberElement);
 
-        // create input type number and ad it to the form
-        const numberElement = document.createElement('input');
-        numberElement.setAttribute('class', 'numberInput');
-        numberElement.setAttribute('type', 'number');
-        // numberElement.setAttribute('value', grid.gridSize);
-        numberElement.setAttribute('min', '4');
-        configElement.appendChild(numberElement);
+        // input type color for pixels
+        // grid.colorPickElement.setAttribute('class', 'colorInput');
+        // grid.colorPickElement.setAttribute('type', 'color');
+        // grid.colorPickElement.setAttribute('value', '#000');
+        // grid.footerElement.appendChild(grid.colorPickElement);
 
+        // input type number for pixels
+        grid.pixelInputElement.setAttribute('class', 'pixelInput');
+        grid.pixelInputElement.setAttribute('type', 'number');
+        grid.pixelInputElement.setAttribute('min', '1');
+        grid.pixelInputElement.setAttribute('max', '100');
+        grid.pixelInputElement.setAttribute('value', '10');
+        grid.configElement.appendChild(grid.pixelInputElement);
 
-        const validationElement = document.createElement('button');
-        validationElement.setAttribute('class', 'validationButton');
-        validationElement.setAttribute('value', 'Apply');
-        validationElement.innerHTML = 'Apply';
-        configElement.appendChild(validationElement);
-        validationElement.addEventListener('click', grid.generateGrid);
+        // grid.pickerElement.style.backgroundColor = 'red';
 
-        configElement.addEventListener('submit', function (event) {
+        // submit button
+        grid.validationElement.setAttribute('class', 'validationButton');
+        grid.validationElement.setAttribute('value', 'Apply');
+        grid.validationElement.setAttribute('type', 'submit');
+        grid.validationElement.innerHTML = 'Apply';
+        grid.configElement.appendChild(grid.validationElement);
 
-            // // prevent default behavior on form submition
-            // console.log(event.target);
-            event.preventDefault();
-            grid.gridSize = parseInt(numberElement.value);
-        });
-
+        // trigger handleSubmit function on submitting form
+        grid.configElement.addEventListener('submit', this.handleSubmit);
     },
+
+    handleSubmit: function (event) {
+        event.preventDefault();
+        grid.gridSize = parseInt(grid.numberElement.value);
+        let getPixelSize = parseInt(grid.pixelInputElement.value);
+        grid.pixelSize = getPixelSize += 'px';
+        grid.generateGrid();
+    },
+
     generateGrid: function () {
         grid.resetGrid();
-        //insert table element
-        const invaderElement = document.getElementById('invader');
-
         // Creates grid
+        console.log(grid.redElement, 'im here');
+        grid.bluevioletElement.addEventListener('click', grid.handlePickerClick);
+        grid.blackElement.addEventListener('click', grid.handlePickerClick);
+
         for (i = 0; i < grid.gridSize; i++) {
-            // debugger;
             const rowElement = document.createElement('div');
             rowElement.className = 'rowElement';
 
             for (j = 0; j < grid.gridSize; j++) {
                 const cellElement = document.createElement('div');
                 cellElement.className = 'cellElement';
+                cellElement.style.height = grid.pixelSize;
+                cellElement.style.width = grid.pixelSize;
                 rowElement.appendChild(cellElement);
-                cellElement.addEventListener('mouseup', grid.handleCellClic
-                )
-            }
-            invaderElement.appendChild(rowElement);
+                cellElement.addEventListener('click', grid.handleCellClic)
+            };
+            grid.invaderElement.appendChild(rowElement);
         };
+        console.log(grid.redElement);
+
     },
+
+    handlePickerClick: function (event) {
+        const clickedPicker = event.target;
+        console.log('there is your clicked picker', clickedPicker);
+        const pickerClass = clickedPicker.className;
+        console.log('your returned picker class is', pickerClass);
+        grid.splitedClass = pickerClass.split(' ');
+        console.log('splited class here ', grid.splitedClass);
+
+        return grid.splitedClass;
+    },
+
     handleCellClic: function (event) {
         const clickedElement = event.target;
-        clickedElement.classList.toggle('cell--black');
-        // if (clickedElement.classList.contains("cell--black")) {
-        //     clickedElement.classList.remove("cell--black");
-        // }
-        // else {
-        //     clickedElement.classList.add("cell--black");
-        // }
+        clickedElement.classList.toggle(grid.splitedClass[1]);
+        console.log('test returned picker', grid.splitedClass[1]);
+    },
+
+    // applyColor: function (event) {
+    //     something
+    // },
+
+    init: function () {
+        grid.generateInput();
+        grid.generateGrid();
     }
 }
 
-
-let init = function () {
-
-    grid.generateInput();
-    grid.generateGrid();
-}
-
-init();
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM fully loaded and parsed');
+    grid.init();
+});
